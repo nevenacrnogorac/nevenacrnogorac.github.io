f2bdd8ca60d452307c578e2ff1cb9bab4b3bae01
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-02-13 22:36:54 +0100

    test first page

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 w-full items-center justify-center font-mono text-3xl lg:flex">
+          <h1> Nevena Crnogorac - Coming soon</h1>
+      </div>
+    </main>
+  );
+}
