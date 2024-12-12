95b8d0e938aef017727d43ed5c36bf792644b9c5
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-12 20:42:36 +0100

    add items to gallery

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const GalleryItem = ({ item }) => {
     return (
         <div className="card bg-primary w-full shadow-2xl">
-            <a href={item.url}>
-            <figure>
+            <a className="grid flex-1 items-end justify-end  divide-y-2" href={item.url}>
+            <figure className="w-full py-1">
                 <img
                     src={item.image}
                     alt={item.name}/>
             </figure>
-            <div className="card-body">
+            <div className="card-body flex-none">
                 <h2 className="card-title">
                     <div className="flex-1">{item.name}</div>
                     {item.status === "NEW" ? (
