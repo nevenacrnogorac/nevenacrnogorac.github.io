d2a8f270e7432861596bacd2e9c4fb50950876ff
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-13 00:38:51 +0100

    back to tree columns

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -2,25 +2,18 @@ import React from "react";
 
 const GalleryItem = ({ item }) => {
     return (
-        <div className="card bg-primary w-full shadow-2xl">
-            <a className="grid flex-1 items-end justify-end  divide-y-2" href={item.url}>
+        <div className="card bg-primary w-full">
+            <a className="grid flex-1 items-end justify-end" href={item.url}>
             <figure className="w-full py-1">
                 <img
+                    className="border-black bordered border-2"
                     src={item.image}
                     alt={item.name}/>
             </figure>
             <div className="card-body flex-none">
-                <h2 className="card-title">
-                    <div className="flex-1">{item.name}</div>
-                    {item.status === "NEW" ? (
-                        <div className="badge badge-primary">NEW</div>
-                    ) : (
-                        <div className="badge bedge-seconady">SOLD</div>)}
-
+                <h2 className="card-title justify-center">
+                    <div className="flex flex-1 justify-center hover:underline">{item.name}</div>
                 </h2>
-                <div className="card-actions justify-start">
-                    <div className="badge badge-outline">{item.category}</div>
-                </div>
             </div>
             </a>
         </div>
