5c11aea285b2a8883344908f14aeeff68d2dc7da
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-13 00:19:03 +0100

    two columns

--- src/layouts/GalleryLayout.jsx
+++ src/layouts/GalleryLayout.jsx
@@ -6,7 +6,7 @@ const GalleryLayout  = ({children}) => {
     return (
         <div>
         <div className="text-2xl items-center flex justify-center p-10"><h1>All artworks</h1></div>
-            <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 justify-center justify-items-center px-10">
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-20 justify-center justify-items-center px-10">
                 {children}
             </div>
         </div>
