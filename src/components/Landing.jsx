3c38da2667655c1734b03948527fd2bba19b4f7d
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-12 16:46:45 +0100

    fix ui

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,17 +5,19 @@ import myImage from '../assets/images/cover.jpg'; // Prilagodite putanju
 const Landing = () => {
     return (
         <div className="hero min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero-content flex-col lg:flex-row-reverse ">
                 <img
                     src={myImage.src}
-                    className="max-w-lg rounded-lg shadow-2xl"/>
+                    className="lg:max-w-2xl rounded-lg shadow-2xl"/>
                 <div className="">
                     <h1 className="text-5xl font-bold">Art Gallery</h1>
                     <p className="py-6">
                         Here you can buy created by Nevena Crnogorac, artist from Serbia.
                     </p>
+                    <div className="grid lg:grid-cols-2 sm:grid-rows-2 gap-2">
                     <button className="btn btn-primary">Browse Gallery</button>
-                    <button className="btn ml-6 btn-outline">Subscribe to email list</button>
+                    <button className="btn btn-outline sm:mx-4">Subscribe to email list</button>
+                    </div>
                 </div>
             </div>
         </div>
