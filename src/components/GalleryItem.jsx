31d1bf9b3e2228cc1db1a98969900a8bded08728
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-13 01:04:04 +0100

    add bedge

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -4,13 +4,16 @@ const GalleryItem = ({ item }) => {
     return (
         <div className="card bg-primary w-full">
             <a className="grid flex-1 items-end justify-end" href={item.url}>
-            <figure className="w-full border-image-border bordered border-4">
-                <img
-                    className="border-black bordered border-2"
-                    src={item.image}
-                    alt={item.name}/>
-            </figure>
-            <div className="card-body flex-none">
+                <figure className="w-full border-image-border bordered border-4 relative">
+                    <img
+                        className="border-black bordered border-2"
+                        src={item.image}
+                        alt={item.name}/>
+                    {item.status == 'SOLD' ?
+                        <div className="badge badge-secondary absolute bottom-2 left-2 rounded-md">Sold</div> :
+                        <div className="badge badge-primary absolute bottom-2 left-2 rounded-md">Available</div>}
+                </figure>
+                <div className="card-body flex-none">
                 <h2 className="card-title justify-center">
                     <div className="flex flex-1 justify-center hover:underline">{item.name}</div>
                 </h2>
