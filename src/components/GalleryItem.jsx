557279d7ac342b58ea169c71b68a20dd4de942d6
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-13 01:11:08 +0100

    add bedge

--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
 const GalleryItem = ({ item }) => {
+    let ele = ''
+    if (item.status === 'SOLD') {
+        ele = <div className="badge badge-secondary absolute bottom-2 left-2 rounded-md">Sold</div>
+    }
+
     return (
         <div className="card bg-primary w-full">
             <a className="grid flex-1 items-end justify-end" href={item.url}>
                 <figure className="w-full border-image-border bordered border-4 relative">
                     <img
-                        className="border-black bordered border-2"
+                        className="border-stone-700 bordered border-2"
                         src={item.image}
                         alt={item.name}/>
-                    {item.status == 'SOLD' ?
-                        <div className="badge badge-secondary absolute bottom-2 left-2 rounded-md">Sold</div> :
-                        <div className="badge badge-primary absolute bottom-2 left-2 rounded-md">Available</div>}
+
+                    {ele}
                 </figure>
                 <div className="card-body flex-none">
                 <h2 className="card-title justify-center">
