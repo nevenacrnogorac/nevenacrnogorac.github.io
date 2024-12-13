ae401b47132392d0d1dcfa3f716f928353858cb8
Nemanja Mikic <nemanja.mikic@oracle.com>
2024-12-13 15:39:45 +0100

    add footer and  load data from json

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer bg-primary flex justify-center bottom-0 divide-y-2">
+            <hr />
+            <div className="footer-center flex justify-center content-center flex-1 pt-4">
+                © 2024, <a href="#">Nevena Crnogorac</a>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/layouts/GalleryLayout.jsx
+++ src/layouts/GalleryLayout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import myImage from '../assets/images/cover.jpg'; // Prilagodite putanju
+import artworks from '../data/artworks.json'
+import GalleryItem from "../components/GalleryItem.jsx";
 
 
-const GalleryLayout  = ({children}) => {
+const GalleryLayout  = () => {
     return (
         <div>
         <div className="text-2xl items-center flex justify-center p-10"><h1>All artworks</h1></div>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 justify-center justify-items-center px-10">
-                {children}
+                {artworks.map((item) => (
+                    <GalleryItem item={item} />
+                ))}
             </div>
         </div>
     );
