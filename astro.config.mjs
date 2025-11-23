a27f14deb388adcbbc25467c79a5d800cfd5f205
Nemanja Mikic <n0t_l3ss@outlook.com>
2025-11-23 17:21:39 +0100

    improve sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,25 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nevenacrnogorac.github.io',
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind(), sitemap({
+    i18n: {
+      defaultLocale: 'en',
+      locales: {
+        en: 'en',
+      },
+    },
+    serialize(item) {
+      if (item.url === 'https://nevenacrnogorac.github.io/') {
+        item.changefreq = 'weekly';
+        item.priority = 1.0;
+      } else if (item.url === 'https://nevenacrnogorac.github.io/gallery/') {
+        item.changefreq = 'weekly';
+        item.priority = 0.8;
+      } else {
+        item.changefreq = 'weekly';
+        item.priority = 0.7;
+      }
+      return item;
+    },
+  })],
 })
